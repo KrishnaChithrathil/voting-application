5f406cded3f9d0aa9edb26c79e8ea0b78392157e
Krishna C <Krishna.Chithrathil@marlabs.com>
2020-10-12 15:22:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Subjects} from './Subjects';
+import {Navigation} from './Navigation';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {NewSub} from './NewSub';
+import {Pass} from './Pass';
+import WinPass from './WinPass';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <BrowserRouter>
+      <div className="container">
+          <h3 className="m-3 d-flex justify-content-center">React Voting Application</h3>
+          <Navigation/>
+          <Route path='/Subjects' component={Subjects}/>
+          <Route path='/Vote' component={Pass}/>
+          <Route path='/WinPass' component={WinPass}/>
+          <Route path='/NewSub' component={NewSub}/>
+      </div>
+      </BrowserRouter>
+      
+    );
+  }
 
 export default App;
